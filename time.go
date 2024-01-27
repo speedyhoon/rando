c91d142f644d929e475d822dbc6191a07dcee693
Cam <16303791+speedyhoon@users.noreply.github.com>
2024-01-27 13:47:35 +1100

    Rename DateTime() to Time(). Move bool functions into bool.go & time functions into time.go.

--- time.go
+++ time.go
@@ -8,3 +8,13 @@ import (
 func Duration(min, max time.Duration) time.Duration {
 	return time.Duration(rand.Intn(int(max-min))) + min
 }
+
+// Time returns a random year, month, day, hour, minute, second.
+func Time() time.Time {
+	return time.Unix(Int64(), 0).UTC()
+}
+
+// TimeNano returns a random year, month, day, hour, minute, second & nanosecond.
+func TimeNano() time.Time {
+	return time.Unix(0, Int64()).UTC()
+}
