1bc67d8e39d8f50a611f099348dec41c99d855c3
Cam <16303791+speedyhoon@users.noreply.github.com>
2024-04-04 01:10:59 +1100

    Rename Bytes() to BytesNil() & add a new Bytes() function that returns non-nil slices.

--- slice.go
+++ slice.go
@@ -2,10 +2,21 @@ package rando
 
 import "math/rand"
 
-func Bytes() (b []byte) {
+// BytesNil returns nil if the random length equals zero.
+func BytesNil() (b []byte) {
 	length := rand.Intn(100)
 	for i := 0; i < length; i++ {
 		b = append(b, Uint8())
+	}
+	return
+}
+
+// Bytes returns []byte{} if the random length equals zero.
+func Bytes() (b []byte) {
+	length := Uint8()
+	b = make([]byte, length)
+	for i := range b {
+		b[i] = Uint8()
 	}
 	return
 }
