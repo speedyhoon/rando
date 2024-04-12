bbe7d2a35303e0a28b8b26681f5298e67c8536f8
Cam <16303791+speedyhoon@users.noreply.github.com>
2024-04-12 16:26:51 +1000

    Create a slice with set length, populated with random values for types float32, float64, int, int8, int16, int32, int64, uint, uint8, uint16, uint32 and uint64.

--- uints.go
+++ uints.go
@@ -19,10 +19,26 @@ func Uints() (u []uint) {
 	return
 }
 
+func UintsN(qty int) (u []uint) {
+	u = make([]uint, qty)
+	for i := 0; i < qty; i++ {
+		u[i] = Uint()
+	}
+	return
+}
+
 func Uint8s() (u []uint8) {
 	l := rand.Intn(defaultMax) + 1
 	u = make([]uint8, l)
 	for i := 0; i < l; i++ {
+		u[i] = Uint8()
+	}
+	return
+}
+
+func Uint8sN(qty int) (u []uint8) {
+	u = make([]uint8, qty)
+	for i := 0; i < qty; i++ {
 		u[i] = Uint8()
 	}
 	return
@@ -37,6 +53,14 @@ func Uint16s() (u []uint16) {
 	return
 }
 
+func Uint16sN(qty int) (u []uint16) {
+	u = make([]uint16, qty)
+	for i := 0; i < qty; i++ {
+		u[i] = Uint16()
+	}
+	return
+}
+
 func Uint32s() (u []uint32) {
 	l := rand.Intn(defaultMax) + 1
 	u = make([]uint32, l)
@@ -46,10 +70,26 @@ func Uint32s() (u []uint32) {
 	return
 }
 
+func Uint32sN(qty int) (u []uint32) {
+	u = make([]uint32, qty)
+	for i := 0; i < qty; i++ {
+		u[i] = Uint32()
+	}
+	return
+}
+
 func Uint64s() (u []uint64) {
 	l := rand.Intn(defaultMax) + 1
 	u = make([]uint64, l)
 	for i := 0; i < l; i++ {
+		u[i] = Uint64()
+	}
+	return
+}
+
+func Uint64sN(qty int) (u []uint64) {
+	u = make([]uint64, qty)
+	for i := 0; i < qty; i++ {
 		u[i] = Uint64()
 	}
 	return
