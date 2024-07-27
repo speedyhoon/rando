6746a78bf968eb3db74af42a77ec9bde8f74650a
sh <16303791+speedyhoon@users.noreply.github.com>
2024-07-27 17:10:55 +1000

    Add BoolsN().

--- bool.go
+++ bool.go
@@ -16,8 +16,13 @@ func Bool10Percent() bool {
 
 // Bools returns a random length []bool populated with random values.
 func Bools() (b []bool) {
-	if u := Uint8n(100); u != 0 {
-		b = make([]bool, u)
+	return BoolsN(Uint8n(100))
+}
+
+// BoolsN returns a []bool with length `size`, populated with random values.
+func BoolsN(size uint8) (b []bool) {
+	if size >= 1 {
+		b = make([]bool, size)
 		for i := range b {
 			b[i] = Bool()
 		}
