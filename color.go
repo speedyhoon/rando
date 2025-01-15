afa7624c33e3d03c59f670a8f6ba9344a6b536af
sh <16303791+speedyhoon@users.noreply.github.com>
2025-01-16 01:37:49 +1100

    Add a function to return a random web-safe colour.

---
+++ color.go
@@ -0,0 +1,10 @@
+package rando
+
+import "github.com/speedyhoon/clr"
+
+// C16 returns a random 16-bit color from 27 possible web-safe primary colors.
+// Each red, green and blue channel uses either 0, 7 or F as their hexadecimal digits.
+func C16() clr.C16 {
+	c := [3]clr.C16{0, 7, 0xF}
+	return c[Uint8n(3)]<<12 | c[Uint8n(3)]<<8 | c[Uint8n(3)]<<4 | 0xf
+}
--- go.mod
+++ go.mod
@@ -1,3 +1,9 @@
 module github.com/speedyhoon/rando
 
-go 1.21.4
+go 1.23
+
+toolchain go1.23.4
+
+require github.com/speedyhoon/clr v0.0.0-20250115091128-89f2f467c845
+
+require github.com/speedyhoon/cnst/clrn v0.0.0-20250113035638-6d85be9cd736 // indirect
