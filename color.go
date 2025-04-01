f633440e446db60a28490dfd91eb7e83d688cf7b
sh <16303791+speedyhoon@users.noreply.github.com>
2025-04-02 00:36:12 +1100

    Add a comment to clarify the array options.

--- color.go
+++ color.go
@@ -5,6 +5,6 @@ import "github.com/speedyhoon/clr"
 // C16 returns a random 16-bit color from 27 possible web-safe primary colors.
 // Each red, green and blue channel uses either 0, 7 or F as their hexadecimal digits.
 func C16() clr.C16 {
-	c := [3]clr.C16{0, 7, 0xF}
+	c := [3]clr.C16{0x0, 0x7, 0xF} // Options to randomly select color values from.
 	return c[Uint8n(3)]<<12 | c[Uint8n(3)]<<8 | c[Uint8n(3)]<<4 | 0xf
 }
